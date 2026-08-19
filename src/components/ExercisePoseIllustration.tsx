985e92b78b574707153fc403a42a95386276fc64
remy00trafford-hash <remy00trafford@gmail.com>
2026-08-19 17:14:58 +0200

    fix: preserve exercise motion exports

--- src/components/ExercisePoseIllustration.tsx
+++ src/components/ExercisePoseIllustration.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PremiumExerciseIllustrationV2 } from "./PremiumExerciseIllustrationV2";
-import { classifyExerciseMotion, EXERCISE_MOTIONS } from "./PremiumExerciseIllustration";
+import { classifyExerciseMotion, EXERCISE_MOTIONS as PREMIUM_EXERCISE_MOTIONS } from "./PremiumExerciseIllustration";
 
 export type PoseCategory = "push" | "pull" | "squat" | "lunge" | "core" | "hinge" | "cardio" | "stretch" | "shoulder" | "arm";
 
@@ -24,5 +24,5 @@ export const ExercisePoseIllustration: React.FC<ExercisePoseIllustrationProps> =
   <PremiumExerciseIllustrationV2 exerciseId={exerciseId} exerciseName={exerciseName} muscleGroup={muscleGroup} />
 );
 
-export const EXERCISE_MOTIONS = EXERCISE_MOTIONS;
+export const EXERCISE_MOTIONS = PREMIUM_EXERCISE_MOTIONS;
 export { classifyExerciseMotion };
