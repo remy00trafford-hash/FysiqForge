7e47c392a234ab9accc7270476a29e58b794f345
remy00trafford-hash <remy00trafford@gmail.com>
2026-08-29 12:23:43 +0200

    fix: expose master exercise library on media branch

---
+++ src/data/masterExerciseDatabase.ts
@@ -0,0 +1,3 @@
+/** Master exercise library is maintained on feat/master-exercise-library-600. This bridge file intentionally re-exports it for the media branch. */
+export { MASTER_EXERCISE_DATABASE, MASTER_EXERCISE_BASE_COUNT, MASTER_EXERCISE_COUNT } from "./masterExerciseDatabase";
+export type { MasterExercise, MasterExerciseCategory, ExerciseLevel, PrescriptionProfile } from "./masterExerciseDatabase";
